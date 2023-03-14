197174142e8a0bbd76fade49ff4e1f8777ce8b27
koumishop15 <koumishop@gmail.com>
2023-03-14 17:13:03 +0100

    feat: create the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { Icon } from '@iconify/react';
+
+export default function Header({hasSignedIn,}) {
+  const [user, setUser] = useState({});
+  const router = useRouter();
+  useEffect(() => {
+    // Perform localStorage action
+    setUser({id:localStorage.getItem('id'), firstName:localStorage.getItem('firstName'), company:localStorage.getItem('company')});
+  }, [])
+
+const handleLogOut = ()=>{
+  console.log(`${user.firstName} logged out`);
+  window.localStorage.clear();
+  router.push('/');
+  
+};
+
+  return (
+    <nav className='p-3 md:py-6 md:px-4 bg-primary flex relative justify-between'>
+      <div className='w-1/4 md:w-[10%]'>
+        <Image src="/images/logoKoumi.png" alt='match-making logo' width={52} height={52} />
+      </div>
+      {hasSignedIn?
+          <div className='w-[60%] md:w-[40%] flex justify-end items-center '>
+            <Icon icon="mdi:user-circle-outline" color='#FFFFFF' width={35} className='text-white' />
+            <div className='text-xs md:text-base md:px-1 flex text-white'><span className='hidden md:flex md:mr-1'>Bonjour </span> <span className='font-extrabold mr-1'>{ user.firstName }</span>{user.company? <span> - <span className='font-extrabold'>{ ` ${user.company}` }</span></span> : ""}</div>
+            <button onClick={handleLogOut} className='flex md:border md:border-secondary md:rounded-xl md:bg-secondary hover:bg-opacity-0 md:px-2 md:ml-4'><span className='hidden md:flex md:text-white'>Déconnexion</span></button>
+            <button onClick={handleLogOut} className='ml-4 md:hidden'>
+              <Icon icon="ic:baseline-logout" width={25} className='md:hidden text-white' />
+            </button>
+          </div> 
+        :
+          <></>
+        }
+    </nav>
+  );
+}
