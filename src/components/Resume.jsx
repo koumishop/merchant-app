24b4c41ca93c1c3560f31714534ed10f1d664544
koumishop15 <koumishop@gmail.com>
2023-03-17 16:38:00 +0100

    feat: create dashboard seller details

---
+++ src/components/Resume.jsx
@@ -0,0 +1,29 @@
+import { Icon } from '@iconify/react'
+
+export default function Resume({ itemsAmount, orderAmount, cashAmount }) {
+return(
+    <div className='md:w-1/2 py-4 flex items-center justify-evenly'>
+        <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
+            <div className='bg-blue border border-blue w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="mdi:cart" width={43} height={43}/></div>
+            <div className='w-full md:w-1/2 text-blue'>
+                <h2>Commandes</h2>
+                <div className='font-semibold'>{orderAmount}</div>
+            </div>
+        </div>
+        <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
+            <div className='bg-green border border-green w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="fa6-solid:boxes-packing" width={43} height={43}/></div>
+            <div className='w-1/2 text-green'>
+                <h2>Produits</h2>
+                <div className='font-semibold'>{itemsAmount}</div>
+            </div>
+        </div>
+        <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
+            <div className='bg-pink-red border border-pinkbg-pink-red w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="ic:round-account-balance-wallet" width={43} height={43}/></div>
+            <div className='w-1/2 text-pink-red'>
+                <h2>Solde</h2>
+                <div className='font-semibold'>{cashAmount}</div>
+            </div>
+        </div>
+    </div>
+    )
+}
