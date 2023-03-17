2ffd37c8955acb5851fdfe147e3c4e17842fb847
koumishop15 <koumishop@gmail.com>
2023-03-17 16:51:02 +0100

    feat: add a link to orders

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,15 +1,18 @@
 import { Icon } from '@iconify/react'
+import Link from 'next/link'
 
 export default function Resume({ itemsAmount, orderAmount, cashAmount }) {
 return(
     <div className='md:w-1/2 py-4 flex items-center justify-evenly'>
-        <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
-            <div className='bg-blue border border-blue w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="mdi:cart" width={43} height={43}/></div>
-            <div className='w-full md:w-1/2 text-blue'>
-                <h2>Commandes</h2>
-                <div className='font-semibold'>{orderAmount}</div>
+        <Link href="/dashboard/orders" className='md:w-[25%]'>
+            <div className='md:w-[100%] md:flex md:space-x-2 space-y-2'>
+                <div className='bg-blue border border-blue w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="mdi:cart" width={43} height={43}/></div>
+                <div className='w-full md:w-1/2 text-blue'>
+                    <h2>Commandes</h2>
+                    <div className='font-semibold'>{orderAmount}</div>
+                </div>
             </div>
-        </div>
+        </Link>
         <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
             <div className='bg-green border border-green w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="fa6-solid:boxes-packing" width={43} height={43}/></div>
             <div className='w-1/2 text-green'>
