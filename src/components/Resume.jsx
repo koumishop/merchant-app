d1d2566e04a07386003cba42df29679b811f04fd
koumishop15 <koumishop@gmail.com>
2023-03-17 17:57:23 +0100

    feat: add product route

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -13,13 +13,15 @@ return(
                 </div>
             </div>
         </Link>
-        <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
-            <div className='bg-green border border-green w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="fa6-solid:boxes-packing" width={43} height={43}/></div>
-            <div className='w-1/2 text-green'>
-                <h2>Produits</h2>
-                <div className='font-semibold'>{itemsAmount}</div>
+        <Link href="/dashboard/products" className='md:w-[25%]'>
+            <div className='md:w-[100%] md:flex md:space-x-2 space-y-2'>
+                <div className='bg-green border border-green w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="fa6-solid:boxes-packing" width={43} height={43}/></div>
+                <div className='w-1/2 text-green'>
+                    <h2>Produits</h2>
+                    <div className='font-semibold'>{itemsAmount}</div>
+                </div>
             </div>
-        </div>
+        </Link>
         <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
             <div className='bg-pink-red border border-pinkbg-pink-red w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="ic:round-account-balance-wallet" width={43} height={43}/></div>
             <div className='w-1/2 text-pink-red'>
