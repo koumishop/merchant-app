7074b7ad5feca7bd7119ade7bc5e3267160dba96
koumishop15 <koumishop@gmail.com>
2023-03-23 11:44:55 +0100

    feat: set resume icon white color

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -36,7 +36,7 @@ return(
     <div className='md:w-1/2 py-4 flex items-center justify-evenly'>
         <Link href="/dashboard/orders" className='md:w-[25%]'>
             <div className='md:w-[100%] md:flex md:space-x-2 space-y-2'>
-                <div className='bg-blue border border-blue w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="mdi:cart" width={43} height={43}/></div>
+                <div className='bg-blue border border-blue w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon className='text-white' icon="mdi:cart" width={43} height={43}/></div>
                 <div className='w-full md:w-1/2 text-blue'>
                     <h2>Commandes</h2>
                     <div className='font-semibold'>{statsData?.total_orders}</div>
@@ -45,7 +45,7 @@ return(
         </Link>
         <Link href="/dashboard/products" className='md:w-[25%]'>
             <div className='md:w-[100%] md:flex md:space-x-2 space-y-2'>
-                <div className='bg-green border border-green w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="fa6-solid:boxes-packing" width={43} height={43}/></div>
+                <div className='bg-green border border-green w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon className='text-white' icon="fa6-solid:boxes-packing" width={43} height={43}/></div>
                 <div className='w-1/2 text-green'>
                     <h2>Produits</h2>
                     <div className='font-semibold'>{statsData?.total_products}</div>
@@ -53,7 +53,7 @@ return(
             </div>
         </Link>
         <div className='md:w-[25%] md:flex md:space-x-2 space-y-2'>
-            <div className='bg-pink-red border border-pinkbg-pink-red w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon icon="ic:round-account-balance-wallet" width={43} height={43}/></div>
+            <div className='bg-pink-red border border-pinkbg-pink-red w-[57px] h-[57px] flex items-center justify-center rounded-md'><Icon className='text-white' icon="ic:round-account-balance-wallet" width={43} height={43}/></div>
             <div className='w-1/2 text-pink-red'>
                 <h2>Solde</h2>
                 <div className='font-semibold'>{`${kFormatter(parseInt(statsData?.balance))} ${statsData?.currency}`}</div>
